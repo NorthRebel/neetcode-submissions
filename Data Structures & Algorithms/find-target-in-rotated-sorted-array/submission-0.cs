1b8e2c4efaa525ec857a9dfcbf4a240aa1d6e7d2
Evgeniy Zyatyna <evgeniy.zyatyna@gmail.com>
2026-08-25 14:43:40 +0300

    Add: find-target-in-rotated-sorted-array - submission-0

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-0.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        var left = 0;
+        var right = nums.Length - 1;
+
+        while (left <= right) {
+            var mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[left]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid - 1;
+                }
+                else {
+                    left = mid + 1;
+                }
+            }
+            else {
+                if (nums[mid] <= target && target <= nums[right]) {
+                    left = mid + 1;
+                }
+                else {
+                    right = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+}
